932c013d4991b79883c6d380e625b135c4d74a87
Anders Pitman <tapitman11@gmail.com>
2016-03-30 10:10:16 -0700

    Add gradle config and example Die class with test

---
+++ src/main/java/Die.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Die {
+    private Random rand;
+
+    public Die() {
+        rand = new Random();
+    }
+
+    public int roll() {
+        return rand.nextInt(6) + 1;
+    }
+}
---
+++ src/test/java/TestDie.java
@@ -0,0 +1,21 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class TestDie {
+    private Die die;
+
+    @Before
+    public void setUp() {
+        die = new Die();
+    }
+
+    @Test
+    public void testRoll() {
+        // Try 1000 rolls and verify they are always in the proper range
+        for (int rollIdx = 0; rollIdx < 1000; rollIdx++) {
+            int roll = die.roll();
+            assertTrue(roll >= 1 && roll <= 6);
+        }
+    }
+}
