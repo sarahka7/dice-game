d0ad09b140e305206dfa7f552b0dbaa41a704811
Nguyen Dao <ndao3@asu.edu>
2016-04-12 22:59:44 -0700

    add Import dice.*

--- gui/src/main/java/DiceGameGUI2.java
+++ gui/src/main/java/DiceGameGUI2.java
@@ -3,11 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package DiceGameUI;
 
 import javax.swing.*;
 import java.util.*;
-
+import dice*;
 /**
  *
  * @author Nguyen
